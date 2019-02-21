251a282d34c1f4a8aceecd06495a23e54aa19b9c
alexyoung <alexyoung612@gmail.com>
2019-02-20 21:23:16 -0800

    added cognito-based authentication

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="login">
+        <div v-if="!signedIn">
+            <amplify-authenticator></amplify-authenticator>
+        </div>
+        <div v-if="signedIn">
+            <amplify-sign-out></amplify-sign-out>
+        </div>
+    </div>
+</template>
+<script>
+import { Auth } from 'aws-amplify';
+import { AmplifyEventBus } from 'aws-amplify-vue';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      signedIn: false,
+    };
+  },
+  created() {
+    this.findUser();
+
+    AmplifyEventBus.$on('authState', (info) => {
+      if (info === 'signedIn') {
+        this.findUser();
+      } else {
+        this.signedIn = false;
+      }
+    });
+  },
+  methods: {
+    async findUser() {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        this.signedIn = true;
+        console.log(user);
+      } catch (err) {
+        this.signedIn = false;
+      }
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false;
 
