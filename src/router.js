8c8d8aae6d4332f3b5dd724f885aeb384664c65d
Alex Young <alexyoung612@gmail.com>
2019-02-22 22:11:03 -0800

    added path authentication and test api call

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Quotes from './views/Quotes.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -26,6 +26,31 @@ export default new Router({
       path: '/quotes',
       name: 'quotes',
       component: Quotes,
+      meta: { requiresAuth: true },
     },
   ],
 });
+
+router.beforeResolve((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    let user;
+    Vue.prototype.$Amplify.Auth.currentAuthenticatedUser().then((data) => {
+      if (data && data.signInUserSession) {
+        user = data;
+        next();
+      }
+    }).catch((e) => {
+      console.log(e);
+    });
+    if (!user) {
+      next({ path: '/' });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+
+export default router;
--- src/views/Quotes.vue
+++ src/views/Quotes.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="quotes">
     <h1>This is the quotes page</h1>
+    <h2>{{info.data}}</h2>
   </div>
 </template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      info: '',
+    };
+  },
+  mounted() {
+    if (this.$store.state.user) {
+      const jwt = this.$store.state.user
+        .getSignInUserSession()
+        .getIdToken()
+        .getJwtToken();
+
+      const config = {
+        headers: {
+          authorization: jwt,
+        },
+      };
+
+      axios.get('https://eju292snv5.execute-api.us-west-2.amazonaws.com/v1/quotes', config)
+        .then((val) => { this.info = val; })
+        .catch(err => console.log(err));
+    }
+  },
+};
+</script>
