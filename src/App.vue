679aa30d7d56ad6af3558d6bbeafbcc8d24a8e36
Alex Young <alexyoung612@gmail.com>
2019-02-26 22:38:35 -0800

    separated navbar component, centered content

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,25 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-avatar
-        :tile="tile"
-        :size="avatarSize"
-        color="grey lighten-4"
-      >
-        <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar!">
-      </v-avatar>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Quotera</span>
-      </v-toolbar-title>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn to="/" flat>Home</v-btn>
-        <v-btn to="/about" flat>About</v-btn>
-        <v-btn to="/quotes" flat>Quotes</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
+    <NavBar/>
     <v-content>
-      <router-view/>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <router-view/>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
-
+    NavBar,
   },
   data() {
     return {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+
+        <v-toolbar app>
+            <v-avatar
+                :tile="tile"
+                :size="avatarSize"
+                color="grey lighten-4"
+            >
+                <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar!">
+            </v-avatar>
+            <v-toolbar-title class="headline text-uppercase mr-3">
+                <span>Quotera</span>
+            </v-toolbar-title>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn
+                v-for="item in menu"
+                :key="item.icon"
+                :to="item.link"
+                flat
+                >{{ item.title }}</v-btn>
+            </v-toolbar-items>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <v-menu class="hidden-md-and-up">
+                <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+                <v-list>
+                <v-list-tile
+                v-for="item in menu"
+                :key="item.icon"
+                :to="item.link"
+                >
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar>
+
+</template>
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      menu: [
+        { icon: 'home', title: 'Home', link: '/' },
+        { icon: 'info', title: 'About', link: '/about' },
+        { icon: 'warning', title: 'Quotes', link: '/quotes' },
+      ],
+      tile: false,
+      avatarSize: 36,
+    };
+  },
+  computed: {
+    signedIn() {
+      return this.$store.state.signedIn;
+    },
+  },
+  methods: {
+    menuItems() {
+      return this.menu;
+    },
+  },
+};
+</script>
